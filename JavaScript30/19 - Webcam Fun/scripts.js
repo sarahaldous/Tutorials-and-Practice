f5da0751b189593b52efb6408c0525020ddb9116
Sarah Aldous <sealdous@gmail.com>
2019-11-12 15:11:07 -0800

    completed webcam tutorial, including searching a deprecated function only to find he had included it in the file

--- JavaScript30/19 - Webcam Fun/scripts.js
+++ JavaScript30/19 - Webcam Fun/scripts.js
@@ -3,3 +3,113 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            //had to do the following line differently, as the way Wes does it has been deprecated
+            video.srcObject = localMediaStream
+            video.play()
+        })
+        .catch(err => {
+            console.error(`OH NO!!!`, err);
+        });
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+        //take the pixels out
+        let pixels = ctx.getImageData(0, 0, width, height);
+        //mess with them
+
+        // pixels = redEffect(pixels);
+
+        // pixels = rgbSplit(pixels)
+        // ctx.globalAlpha = 0.1;
+
+        pixels = greenScreen(pixels);
+
+        //put them back
+        ctx.putImageData(pixels, 0, 0);
+    }, 16);
+}
+
+function takePhoto() {
+    //played the sound
+    snap.currentTime = 0;
+    snap.play();
+
+    //take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt=""Handsome Me" />`
+    strip.insertBefore(link, strip.firstChild)
+}
+
+function redEffect(pixels) {
+    for(let i = 0; i < pixels.data.length; i +=4) {
+        // pixels[i] //red
+        // pixels[i + 1] //green
+        // pixels[i + 2] //blue
+        pixels.data[i + 0] = pixels.data[i + 0] + 200; //red
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; //green
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //blue
+       
+    }
+    return pixels
+}
+
+function rgbSplit(pixels) {
+    for(let i = 0; i < pixels.data.length; i +=4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; //red
+        pixels.data[i + 500] = pixels.data[i + 1]; //green
+        pixels.data[i - 550] = pixels.data[i + 2] 
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+    //holds minimum and max green as the range to remove from image
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+      });
+
+    //   console.log(levels)
+    
+      for (i = 0; i < pixels.data.length; i = i + 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+    
+        if (red >= levels.rmin
+          && green >= levels.gmin
+          && blue >= levels.bmin
+          && red <= levels.rmax
+          && green <= levels.gmax
+          && blue <= levels.bmax) {
+          // take it out!
+          pixels.data[i + 3] = 0;
+        }
+     
+    }
+    return pixels;
+}
+
+
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas)
