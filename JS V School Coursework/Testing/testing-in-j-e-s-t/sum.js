07a9a90f5fadf4a3f05f2da89554e09379d97988
Sarah Aldous <sealdous@gmail.com>
2019-11-14 14:01:36 -0800

    completed JS30 number 22

--- JS V School Coursework/Testing/testing-in-j-e-s-t/sum.js
+++ JS V School Coursework/Testing/testing-in-j-e-s-t/sum.js
@@ -1,6 +1,15 @@
 function sum(a, b) {
+    if(typeof a !== "number" || typeof b !== "number"){
+        return "Please provide 2 numerical values"
+    }
+
+    if(!a || !b){
+        return "Please provide 2 numbers"
+    }
     return a + b
 }
-module.exports = sum
+module.exports = {
+    sum: sum
+}
 // this can be equal to the function OR to an object that contains more functions. example: 
 //module.exports = {sum: sum, otherFunction: otherFunction}
--- JS V School Coursework/Testing/testing-in-j-e-s-t/sum.test.js
+++ JS V School Coursework/Testing/testing-in-j-e-s-t/sum.test.js
@@ -1,6 +1,36 @@
-var { sum } = require('./sum')
+var {sum} = require('./sum')
+console.log(sum)
 // the curly braces around var {sum} are so that it will look inside the object in ./sum for a key called "sum" and name it as a variable. See minutes 29 for more info.
 
-test('adds 1 + 2 to equal 3', function() {
-    expect(sum(1, 2)).toBe(3)
+//describes what all the tests are for. First argument is name of function; second argument wraps all the tests that are included in that describe.
+describe("The Sum Function", function(){
+
+    test('adds 1 + 2 to equal 3', function() {
+        expect(sum(1, 2)).toBe(3)
+    })
+    //for arrays, must use .toEqual, .toContain, etc, not .toBe
+    
+    test("negative numbers give correct output", function() {
+        expect(sum(-1, -2)).toBe(-3)
+        expect(sum(-5, -6)).toBe(-11)
+        expect(sum(-100, -67)).toBe(-167)
+        
+    })
+    
+    test("if no arguments, sends error string", function(){
+        expect(sum()).toBe("Please provide 2 numerical values")
+    })
+
+    instanceof("returns an error if non-numerical values are given", function(){
+        expect(sum(true, "hello")).toBe("Please provide 2 numerical values")
+        expect(sum([], {})).toBe("Please provide 2 numerical values")
+    })
 })
+
+
+//Other things you might need to test for: tooo large of numbers; wrong type(boolean, string, array)
+
+//"it" can be used in place of "test" at the beginning of a test. Example:
+// it("if no arguments, sends error string", function(){
+//     expect(sum()).toBe("Please provide 2 numbers")
+// })
