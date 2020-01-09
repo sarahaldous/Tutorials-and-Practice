c7dc9df7238ca7d75b6b730de72c3f39bd69ec1a
Sarah Aldous <sealdous@gmail.com>
2020-01-08 16:53:08 -0800

    partway through debugging

--- Beginner JavaScript/exercises/16 - Debugging/debugging.js
+++ Beginner JavaScript/exercises/16 - Debugging/debugging.js
@@ -9,16 +9,24 @@ const people = [
 /* Victor Mono */
 // New Font!
 people.forEach((person, index) => {
-  console.log(person.name);
+//   console.groupCollapsed(`${person.name}`); 
+//   console.log(person.country);
+//   console.log(person.cool)
+//   console.log('DONE!')
+//   console.groupEnd(`${person.name}`)
+debugger;
+console.log(person.name)
 });
-
+// console.table(people)
 // console.table(people);
 
 // Console Methods
 
 // Callstack, Stack Trace
+//functions that call subsequent functions in a row. When this happens and there's a problem, the call stack will show each spot where there was a problem. 
 
 // Grabbing Elements
+//Putting $0 in Console tab will bring up what you've selected in the elements tab with your inspect arrow. $1 is the second-to-last element you selected. On pages without jQuery, $ and $$ allow you to select things with query selectors. $ selects one paragraph or whatever; $$ selects all the paragraphs
 
 // Breakpoints
 
@@ -32,7 +40,7 @@ people.forEach((person, index) => {
 
 function doALotOfStuff() {
   console.group('Doing some stuff');
-  console.log('Hey Im one');
+  console.log('Hey I\'m one');
   console.warn('watch out!');
   console.error('hey');
   console.groupEnd('Doing some stuff');
