c283dfdb7bc648acc406e32eb3af349d5fa5b452
Sarah Aldous <sealdous@gmail.com>
2020-01-14 15:41:27 -0800

    selecting elements in the DOM

---
+++ Beginner JavaScript/exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,13 @@
+/*eslint-disable*/
+const p = document.querySelector('p')
+const imgs = document.querySelectorAll('.item img')
+console.log(p)
+console.log(imgs)
+
+//The following is only necessary if you have the script tag in the head of your HTML. If you put it at the end of the body tag, you don't need this:
+// function init() {
+//     const p = document.querySelector('p');
+//     console.log(p)
+// }
+// document.addEventListener('DOMContentLoaded', init);
+
