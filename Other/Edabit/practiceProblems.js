9d93a55a8c64fec8e876dccf78a0a217595ac450
Sarah Aldous <sealdous@gmail.com>
2020-02-04 10:17:41 -0800

    arrays

--- Other/Edabit/practiceProblems.js
+++ Other/Edabit/practiceProblems.js
@@ -10,3 +10,22 @@
 function isSameNum(num1, num2) {
     return num1 === num2 ? true : false
 }
+const arr = ["cars", "planes", ["trains", ["motorcycles"]]]
+
+// Fix the following using es6 destructuring
+// Only edit what's inside of [ trans1, trans2, trans3, trans4 ]
+let [ trans1, trans2, [trans3, [trans4 ]]] = arr
+
+/* 
+console.log(trans1) // should output "cars"
+console.log(trans2) // should output "planes"
+console.log(trans3) // should output "trains"
+console.log(trans4) // should output "motorcycles"
+*/
+
+// Change the string so that it will evaluate to head = 1 and tail = [2, 3, 4] 
+// Use the Rest element 
+const str = `[head, ...tail] = [1, 2, 3, 4]`
+
+// console.log(head) // should output 1
+// console.log(tail) // should output [2, 3, 4]
