cd6c84def97cd20c1594ca12feb35689c72dfe45
Sarah Aldous <sealdous@gmail.com>
2020-02-03 16:46:44 -0800

    strictly equals problem

---
+++ Other/Edabit/practiceProblems.js
@@ -0,0 +1,12 @@
+// Create a function that returns true when num1 is equal to num2.
+
+// Examples
+// isSameNum(4, 8) ➞ false
+
+// isSameNum(2, 2) ➞  true
+
+// isSameNum(2, "2") ➞ false
+
+function isSameNum(num1, num2) {
+    return num1 === num2 ? true : false
+}
