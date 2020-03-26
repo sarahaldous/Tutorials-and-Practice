b36105e85d9925a92f10284a2243fe916e8db347
Sarah Aldous <sealdous@gmail.com>
2020-03-25 21:11:40 -0700

    string length challenge

--- Other/Edabit/practiceProblems.js
+++ Other/Edabit/practiceProblems.js
@@ -178,3 +178,5 @@ function wordLengths(arr) {
 //or
 const wordLengths = arr => arr.map(x => x.length)
 
+//Create a function that takes two strings as arguments and return either true or false depending on whether the total number of characters in the first string is equal to the total number of characters in the second string.
+const comp = (str1, str2) => str1.length === str2.length  
