5c9d48828c081f0ae3f5e9c70bd53d72c8022d6e
sarahaldous <sealdous@hotmail.com>
2020-05-05 15:09:29 -0700

    probability problem

--- Other/Edabit/practiceProblems.js
+++ Other/Edabit/practiceProblems.js
@@ -232,3 +232,8 @@ function numArgs() {
 	return arguments.length;
 }
 
+
+// Create a function that takes in three arguments (prob, prize, pay) and returns true if prob * prize > pay; otherwise return false.
+
+// To illustrate, profitableGamble(0.2, 50, 9) should yield true, since the net profit is 1 (0.2 * 50 - 9), and 1 > 0.
+const profitableGamble = (prob, prize, pay) => prob * prize > pay ? true : false
