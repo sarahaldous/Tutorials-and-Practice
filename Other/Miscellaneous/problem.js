e68025b50f4e95781ba80d616af409197a2b9dc8
sarahaldous <sealdous@hotmail.com>
2020-05-15 17:07:23 -0700

    practicing with sets and arrays

--- Other/Miscellaneous/problem.js
+++ Other/Miscellaneous/problem.js
@@ -7,3 +7,18 @@ function returnEven(arr){
     } return newArr
 }
 console.log(returnEven([1,2,3,4,5,6,7,8,9,10])) // returns [2,4,6,8,10]
+
+const numbers = [2,3,4,4,2,3,3,4,4,5,5,6,6,7,5,32,3,4,5]
+
+console.log([...new Set(numbers)]) 
+let mySet = new Set(numbers)
+console.log(mySet)
+mySet.add(29)
+mySet.add(345)
+mySet.delete(4)
+mySet.has(5)
+// console.log(mySet.has(4))
+// console.log(mySet.has(Math.sqrt(25)))
+// console.log(mySet.size)
+console.log(mySet)
+console.log([...mySet])
