95e65df703c06cb1c1ff9f8bd7cde8cd9eaac98a
Sarah Aldous <sealdous@hotmail.com>
2020-08-24 10:23:56 -0700

    map and indices problem

--- Other/Edabit/practiceProblems.js
+++ Other/Edabit/practiceProblems.js
@@ -782,3 +782,14 @@ function countClaps(str) {
 
 //Create a function that takes a positive integer and returns the nth "star number".
 const starNumber = (n) => (n * (n - 1) * 6) + 1
+
+//Given an array of numbers, create a function which returns the same array but with each element's index in the array added to itself. This means you add 0 to the number at index 0, add 1 to the number at index 1, etc...
+function addIndexes(arr) {
+	let arr2 = []
+			for (let i = 0; i < arr.length; i++){
+		 arr2.push(arr[i] + i)
+	}
+	return arr2
+}
+//or
+const addIndexes = arr => arr.map((number, index) => number + index)
