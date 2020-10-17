77dcd2bb2eb6762ca57fb7c96d284c5bd4fea0c4
Sarah Aldous <sealdous@hotmail.com>
2020-10-16 18:31:10 -0700

    switch statement

--- Other/Edabit/practiceProblems.js
+++ Other/Edabit/practiceProblems.js
@@ -1209,4 +1209,44 @@ function incrementItems(arr) {
 	return arr
 }
 
+// /Create a function that returns a number, based on the string provided. All numbers are single digits.
+function word(s) {
+	switch(s) {
+		case "one":
+			return 1
+			break;
+		case "two":
+			return 2
+			break;	
+		case "three":
+			return 3
+			break;	
+		case "four":
+			return 4
+			break;	
+		case "five":
+			return 5
+			break;	
+		case "six":
+			return 6
+			break;
+		case "seven":
+			return 7
+			break;	
+		case "eight":
+			return 8
+			break;	
+		case "nine":
+			return 9
+			break;	
+		case "zero":
+			return 0
+			break;	
+	}
+}
+//or
+word = s => ["zero","one","two","three","four","five","six","seven","eight","nine"].indexOf(s)
+//or
+const word = n => ({zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9})[n];
+
 
