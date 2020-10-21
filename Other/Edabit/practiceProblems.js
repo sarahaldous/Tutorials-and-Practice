d5f90e7545ccc83f473f5456b0d38ad3d6323661
Sarah Aldous <sealdous@hotmail.com>
2020-10-20 17:04:42 -0700

    constructor calculator

--- Other/Edabit/practiceProblems.js
+++ Other/Edabit/practiceProblems.js
@@ -1249,4 +1249,25 @@ word = s => ["zero","one","two","three","four","five","six","seven","eight","nin
 //or
 const word = n => ({zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9})[n];
 
-
+//Simple OOP Calculator
+class Calculator {
+	add(a, b) {
+		return a + b
+	}
+	subtract(a, b) {
+		return a - b
+	}
+	divide(a, b) {
+		return a / b
+	}
+	multiply(a, b) {
+		return a * b
+	}
+}
+//or
+class Calculator {
+	add = (a, b) => a + b;
+	subtract = (a, b) => a - b;
+	multiply = (a, b) => a * b;
+	divide = (a, b) => a / b;
+}
